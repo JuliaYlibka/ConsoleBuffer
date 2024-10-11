35c20e42912dae6bb2fc0ca0d069b903a6cc4c1f
Безматерных Юлия Александровна <217041@edu.fa.ru>
2024-10-11 09:38:11 +0300

    Добавлено меню для пользовательского выбора и закончена задача

--- ConsoleBuffer/ConsoleBuffer.cpp
+++ ConsoleBuffer/ConsoleBuffer.cpp
@@ -5,68 +5,58 @@
 using namespace std;
 
 int createActiveConsole() {
-    HANDLE hStdOutOld, hStdOutNew; // дескрипторы буфера экрана
-    DWORD dwwritten; // для количества выведенных символов
-    // создаем буфер экрана
+    HANDLE hStdOutOld, hStdOutNew; 
+    DWORD dwwritten; 
+    
     hStdOutNew = CreateConsoleScreenBuffer(
-        GENERIC_READ | GENERIC_WRITE, // чтение и запись
-        0, // не разделяемый
-        NULL, // защита по умолчанию
-        CONSOLE_TEXTMODE_BUFFER, // текстовый режим
-        NULL); // не используется
+        GENERIC_READ | GENERIC_WRITE, 
+        0, 
+        NULL, 
+        CONSOLE_TEXTMODE_BUFFER, 
+        NULL); 
     if (hStdOutNew == INVALID_HANDLE_VALUE) {
         _cputs("Create console screen buffer failed.\n");
         return GetLastError();
     }
-    // сохраняем старый буфер экрана
+    
     hStdOutOld = GetStdHandle(STD_OUTPUT_HANDLE);
-    // ждем команду на переход к новому буферу экрана
+
     _cputs("Press any key to set new screen buffer active.\n");
     _getch();
-    // делаем активным новый буфер экрана
     if (!SetConsoleActiveScreenBuffer(hStdOutNew)) {
         _cputs("Set new console active screen buffer failed.\n");
         return GetLastError();
     }
-    // выводим текст в новый буфер экрана
     char text[] = "This is a new screen buffer.";
     if (!WriteConsole(hStdOutNew, text, sizeof(text) - 1, &dwwritten, NULL)) {
         _cputs("Write console output character failed.\n");
     }
-    // выводим сообщение о вводе символа
     char str[] = "\nPress any key to set old screen buffer.";
     if (!WriteConsole(hStdOutNew, str, sizeof(str) - 1, &dwwritten, NULL)) {
         _cputs("Write console output character failed.\n");
     }
     _getch();
-    // восстанавливаем старый буфер экрана
     if (!SetConsoleActiveScreenBuffer(hStdOutOld)) {
         _cputs("Set old console active screen buffer failed.\n");
         return GetLastError();
     }
-    // пишем в старый буфер экрана
     _cputs("This is an old console screen buffer.\n");
-    // закрываем новый буфер экрана
     CloseHandle(hStdOutNew);
-    // ждем команду на завершение программы
     _cputs("Press any key to finish.\n");
     _getch();
     return 0;
 }
 
 int OutputFromBufferConsole() {
-    HANDLE hStdOut; // дескриптор стандартного вывода
-    CONSOLE_SCREEN_BUFFER_INFO csbi; // для параметров буфера экрана
+    HANDLE hStdOut; 
+    CONSOLE_SCREEN_BUFFER_INFO csbi; 
 
-    // читаем стандартный дескриптор вывода
     hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    // читаем параметры буфера экрана
     if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) {
         cout << "Console screen buffer info failed." << endl;
         return GetLastError();
     }
     cout << "Console screen buffer info: " << endl << endl;
-    // выводим на консоль параметры выходного буфера
     cout << "A number of columns = " << csbi.dwSize.X << endl;
     cout << "A number of rows = " << csbi.dwSize.Y << endl;
     cout << "X cursor coordinate = " << csbi.dwCursorPosition.X << endl;
@@ -80,11 +70,11 @@ int OutputFromBufferConsole() {
 }
 
 int SetNewSizeOfBuffer() {
-    COORD coord; // для размера буфера экрана
-    HANDLE hStdOut; // дескриптор стандартного вывода
-    // читаем дескриптор стандартного вывода
+    COORD coord; 
+    HANDLE hStdOut; 
+    
     hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    // вводим новый размер буфера экрана
+    
     cout << "Enter new screen buffer size." << std::endl;
     cin >> hStdOut;
 
@@ -92,7 +82,7 @@ int SetNewSizeOfBuffer() {
     cin >> coord.X;
     cout << "A number of rows: ";
     cin >> coord.Y;
-    // устанавливаем новый размер буфера экрана
+    
     if (!SetConsoleScreenBufferSize(hStdOut, coord)) {
         cout << "Set console screen buffer size failed." << endl;
         return GetLastError();
@@ -100,20 +90,19 @@ int SetNewSizeOfBuffer() {
     return 0;
 }
 int ReadAndWriteConsole() {
-    HANDLE hStdOut, hStdIn; // дескрипторы консоли
-    DWORD dwWritten, dwRead; // для количества символов
-    char buffer[80]; // для ввода символов
+    HANDLE hStdOut, hStdIn; 
+    DWORD dwWritten, dwRead; 
+    char buffer[80];
     char str[] = "Input any string:";
 
     char c;
-    // читаем дескрипторы консоли
+
     hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
     hStdIn = GetStdHandle(STD_INPUT_HANDLE);
     if (hStdOut == INVALID_HANDLE_VALUE || hStdIn == INVALID_HANDLE_VALUE) {
         cout << "Get standard handle failed." << endl;
         return GetLastError();
     }
-    // выводим сообщения о вводе строки
     if (!WriteConsole(hStdOut, &str, sizeof(str), &dwWritten, NULL)) {
         cout << "Write console failed." << endl;
         return GetLastError();
@@ -122,7 +111,6 @@ int ReadAndWriteConsole() {
         cout << "Read console failed." << endl;
         return GetLastError();
     }
-    // ждем команду на завершение работы
     cout << "Input any char to exit: ";
     cin >> c;
     return 0;
@@ -163,8 +151,6 @@ int main() {
         case 5:
             exit = true;
         }
-
-
         
     }
     return 0;
